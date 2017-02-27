6e5d1c00bf0f0c8ede3616abad392fdb70b430a8
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-02-27 23:09:12 +0100

    added hidden comments to js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@
 var name = prompt('Wpisz swoje imię');
 // alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+/* komentarz wstawiem tutaj
+zeby pokazac jak to sie robie na kilka linijek
+aha i wiem, ze powinien byc po angielsku dammit!
+*/
+// alright, here you go
