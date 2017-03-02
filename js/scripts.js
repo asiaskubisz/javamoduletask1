8b5a900fb8ef44d2a668834556ba31638acdbcc7
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-02 12:19:50 +0100

    added all updated for task 4

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,15 @@ zeby pokazac jak to sie robie na kilka linijek
 aha i wiem, ze powinien byc po angielsku dammit!
 */
 // alright, here you go
-var a = 2;
-var h = 3;
-var triangleArea = a*h/2;
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+var a = 3;
+var b = 4;
+var value = (a*a) + (2 * a * b) - (b*b);
+console.log('Wartość rachunku: ' + value );
+
+if (value < 0) {
+    console.log('Jest to liczba ujemna');
+} else if (value > 0) {
+    console.log('Jest to liczba dodatnia');
+} else if (value == 0) {
+    console.log('Wynik rowna się: zero');
+}
